6acbf8e9f3a743da64905c4714a4b113fb0ddfbd
minhaaz717 <86486466+minhaaz717@users.noreply.github.com>
2022-09-01 12:43:39 -0500

    Add files via upload

---
+++ pythoncode5.py
@@ -0,0 +1,10 @@
+#radius as user input
+radius=float(input("enter the radius of the circle: "))
+
+#calculating area of circle
+_area_of_circle = 3.14*radius*radius
+print("area of circle is: {0}".format(_area_of_circle))
+
+#calculating circumference of circle
+_circumference_of_circle = 2*3.14*radius
+print("circumference of circle is: {0}".format(_circumference_of_circle))
